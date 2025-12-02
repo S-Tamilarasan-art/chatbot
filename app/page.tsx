4be29045590afca02b385e93cbdb4a228f28b785
tamilarasant904@gmail.com <tamilarasant904@gmail.com>
2025-12-02 17:34:14 +0530

    suspense boundary added to fix usesearchParams issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 
 import ChatMessage from "@/components/ChatMessage";
+import { Suspense } from "react";
 export default function Home() {
-  return <ChatMessage />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ChatMessage />;
+    </Suspense>
+  );
 }
